01963eae67516e92fb4cfe2c82f4dc29faf123f0
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-11-30 10:11:45 +0800

    Remove the not needed code.

--- src/phishpediaPKG.py
+++ src/phishpediaPKG.py
@@ -1,8 +1,12 @@
 #-----------------------------------------------------------------------------
 # Name:        PhishperidaPKG.py
 #
-# Purpose:     This module is used to package the Phishperida project as a black
-#              box API for other project to use 
+# Purpose:     This module is used to package the NUS-Phishperida phishing url 
+#              detection project as a black box API for other project to use. 
+#              put this module in the same folder of the NUS-Phishperida's 'main'
+#              module.
+#              NUS-Phishperida link: https://github.com/lindsey98/Phishpedia
+# 
 # Author:      Yuancheng Liu
 #
 # Created:     2020/11/24
@@ -11,17 +15,19 @@
 # License:     n.a
 #-----------------------------------------------------------------------------
 
-
-from phishpedia_config import *
 import os
-import argparse
 import time
+from phishpedia_config import *
 from src.util.chrome import *
-# import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 print("Current working directory is : %s" % os.getcwd())
 DRI_PATH = os.path.dirname(__file__)
 
+# defined the used file name.
+RST_FILE = 'pedia_rlt.txt' # result recode file.
+URL_FILE= 'info.txt' # url file name
+OUT_FILE = "shot.png"
+PREDIC_FILE = "predict.png"
 
 #####################################################################################################################
 # ** Step 1: Enter Layout detector, get predicted elements
@@ -30,7 +36,6 @@ DRI_PATH = os.path.dirname(__file__)
 # **         If Siamese report no target, Return Benign, None
 # **         Else Siamese report a target, Return Phish, phishing target
 #####################################################################################################################
-
 
 def mainRun(url, screenshot_path):
     '''
@@ -51,7 +56,7 @@ def mainRun(url, screenshot_path):
 
     ####################### Step1: layout detector ##############################################
     pred_boxes, _, _, _ = pred_rcnn(im=screenshot_path, predictor=ele_model)
-    pred_boxes = pred_boxes.detach().cpu().numpy()  ## get predicted logo box
+    pred_boxes = pred_boxes.detach().cpu().numpy()  # get predicted logo box
     plotvis = vis(screenshot_path, pred_boxes)
     print("plot")
 
@@ -63,13 +68,13 @@ def mainRun(url, screenshot_path):
 
     ######################## Step2: Siamese (logo matcher) ########################################
     pred_target, matched_coord, siamese_conf = phishpedia_classifier_logo(logo_boxes=pred_boxes,
-                                                                     domain_map_path=domain_map_path,
-                                                                     model=pedia_model,
-                                                                     logo_feat_list=logo_feat_list,
-                                                                     file_name_list=file_name_list,
-                                                                     url=url,
-                                                                     shot_path=screenshot_path,
-                                                                     ts=siamese_ts)
+                                                                          domain_map_path=domain_map_path,
+                                                                          model=pedia_model,
+                                                                          logo_feat_list=logo_feat_list,
+                                                                          file_name_list=file_name_list,
+                                                                          url=url,
+                                                                          shot_path=screenshot_path,
+                                                                          ts=siamese_ts)
 
     if pred_target is None:
         print('Did not match to any brand, report as benign')
@@ -84,31 +89,33 @@ def mainRun(url, screenshot_path):
 
     return phish_category, pred_target, plotvis, siamese_conf
 
-class phishperidaPKG():
 
-    def __init__(self, driverType="C"):
-        self.resultsPath = os.path.join(DRI_PATH, 'pedia_rlt.txt')
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+class phishperidaPKG(object):
+
+    def __init__(self):
+        """ Init the object and create the result record file.
+        """
+        self.resultsPath = os.path.join(DRI_PATH, RST_FILE)
         if not os.path.exists(self.resultsPath):
             titleArr = ('folder', 'url', 'phish', 'prediction', 'siamese_conf', 'vt_result', 'runtime')
             with open(self.resultsPath, "w+") as f:
                 f.write("\t".join(titleArr)+"\n")
    
+   #-----------------------------------------------------------------------------
     def phishperidaCheck(self, directory):        
         for item in tqdm(os.listdir(directory)):
             start_time = time.time()
             try:
                 print(item)
                 full_path = os.path.join(directory, item)
-
-                screenshot_path = os.path.join(full_path, "shot.png")
-                url = open(os.path.join(full_path, 'info.txt'), encoding='ISO-8859-1').read()
-
+                screenshot_path = os.path.join(full_path, OUT_FILE)
+                url = open(os.path.join(full_path, URL_FILE), encoding='ISO-8859-1').read()
                 if not os.path.exists(screenshot_path):
                     continue
-
                 else:
                     phish_category, phish_target, plotvis, siamese_conf = mainRun(url=url, screenshot_path=screenshot_path)
-
                     # FIXME: call VTScan only when phishpedia report it as phishing
                     vt_result = "None"
                     if phish_target is not None:
@@ -120,103 +127,25 @@ class phishperidaPKG():
                             else:
                                 print("Negative VT scan!")
                                 vt_result = "None"
-
                         except Exception as e:
                             print('VTScan is not working...')
                             vt_result = "error"
-
                     # write results as well as predicted images
                     with open(self.resultsPath, "a+", encoding='ISO-8859-1') as f:
-                        f.write(item + "\t")
-                        f.write(url + "\t")
-                        f.write(str(phish_category) + "\t")
-                        f.write(str(phish_target) + "\t")  # write top1 prediction only
-                        f.write(str(siamese_conf) + "\t")
-                        f.write(vt_result + "\t")
-                        f.write(str(round(time.time() - start_time, 4)) + "\n")
-
-                    cv2.imwrite(os.path.join(full_path, "predict.png"), plotvis)
-
+                        f.write('\t'.join((item, url, str(phish_category), str(phish_target), str(
+                            siamese_conf), vt_result, str(round(time.time() - start_time, 4))))+'\n')
+                        # f.write(item + "\t")
+                        # f.write(url + "\t")
+                        # f.write(str(phish_category) + "\t")
+                        # f.write(str(phish_target) + "\t")  # write top1 prediction only
+                        # f.write(str(siamese_conf) + "\t")
+                        # f.write(vt_result + "\t")
+                        # f.write(str(round(time.time() - start_time, 4)) + "\n")
+                    cv2.imwrite(os.path.join(full_path, PREDIC_FILE), plotvis)
             except Exception as e:
                 print(str(e))
 
+#-----------------------------------------------------------------------------
 if __name__ == "__main__":
     checker = phishperidaPKG()
     checker.phishperidaCheck('./datasets/test_sites')
-
-""" if __name__ == "__main__":
-
-    # os.environ["CUDA_VISIBLE_DEVICES"]="1"
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', "--folder", help='Input folder path to parse',  default='./datasets/test_sites')
-    parser.add_argument('-r', "--results", help='Input results file name', default='./test.txt')
-    args = parser.parse_args()
-    date = args.folder.split('/')[-1]
-    directory = args.folder
-    results_path = args.results.split('.txt')[0] + "_pedia.txt"
-
-    if not os.path.exists(results_path):
-        with open(results_path, "w+") as f:
-            f.write("folder" + "\t")
-            f.write("url" + "\t")
-            f.write("phish" + "\t")
-            f.write("prediction" + "\t")  # write top1 prediction only
-            f.write("siamese_conf" + "\t")
-            f.write("vt_result" + "\t")
-            f.write("runtime" + "\n")
-
-    for item in tqdm(os.listdir(directory)):
-        start_time = time.time()
-
-        # if item in open(results_path, encoding='ISO-8859-1').read(): # have been predicted
-        #     continue
-
-        try:
-            print(item)
-            full_path = os.path.join(directory, item)
-
-            screenshot_path = os.path.join(full_path, "shot.png")
-            url = open(os.path.join(full_path, 'info.txt'), encoding='ISO-8859-1').read()
-
-            if not os.path.exists(screenshot_path):
-                continue
-
-            else:
-                phish_category, phish_target, plotvis, siamese_conf = main(url=url, screenshot_path=screenshot_path)
-
-                # FIXME: call VTScan only when phishpedia report it as phishing
-                vt_result = "None"
-                if phish_target is not None:
-                    try:
-                        if vt_scan(url) is not None:
-                            positive, total = vt_scan(url)
-                            print("Positive VT scan!")
-                            vt_result = str(positive) + "/" + str(total)
-                        else:
-                            print("Negative VT scan!")
-                            vt_result = "None"
-
-                    except Exception as e:
-                        print('VTScan is not working...')
-                        vt_result = "error"
-
-                # write results as well as predicted images
-                with open(results_path, "a+", encoding='ISO-8859-1') as f:
-                    f.write(item + "\t")
-                    f.write(url + "\t")
-                    f.write(str(phish_category) + "\t")
-                    f.write(str(phish_target) + "\t")  # write top1 prediction only
-                    f.write(str(siamese_conf) + "\t")
-                    f.write(vt_result + "\t")
-                    f.write(str(round(time.time() - start_time, 4)) + "\n")
-
-                cv2.imwrite(os.path.join(full_path, "predict.png"), plotvis)
-
-        except Exception as e:
-            print(str(e)) """
-
-    #  raise(e)
-time.sleep(2)
-
-
-
